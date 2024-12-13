473c9175cd51b7d08123796f8a3de317570038ea
최상현 <itchoi0429@gmail.com>
2024-12-13 19:27:54 +0900

    first commit

---
+++ src/main/kotlin/com/choisinsa/ChoisinsaWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.choisinsa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChoisinsaWithKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChoisinsaWithKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/choisinsa/ChoisinsaWithKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.choisinsa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChoisinsaWithKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
